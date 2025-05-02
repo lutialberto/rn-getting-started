ece1ea35773d6f3dc27c34a2d01f3202a5d48152
Alberto Luti <alberto_luti@edsa.com.ar>
2025-05-02 18:51:51 -0300

    Fix: remove modal screen + clear tabOneScreen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,11 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { TextApp } from "@/components/texts/TextApp";
-import { Link } from "expo-router";
-import { useForm } from "react-hook-form";
-import InputTextApp from "@/components/forms/inputText/InputTextApp";
 import { ViewThemed } from "@/components/containers/ViewThemed";
 
 export default function TabOneScreen() {
-  const {
-    control,
-    formState: { errors },
-    setValue,
-    handleSubmit,
-  } = useForm<{ name: string }>({
-    defaultValues: {
-      name: "",
-    },
-  });
-
   return (
     <ViewThemed style={styles.container}>
       <TextApp style={styles.title}>Tab One</TextApp>
-      <Link href="/modal">
-        <TextApp>Go to modal</TextApp>
-      </Link>
-      <View style={{ gap: 10 }}>
-        <InputTextApp
-          formControl={{
-            control: control,
-            name: "name",
-            rules: { required: "The input field is required" },
-          }}
-          label={"Nombre"}
-          error={errors.name?.message}
-          containerStyle={{ minWidth: 250 }}
-          clearInput={() => setValue("name", "")}
-        />
-      </View>
     </ViewThemed>
   );
 }
