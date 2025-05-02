083633bcd77894dc3b979521a26d1c940ac227c2
Alberto Luti <alberto_luti@edsa.com.ar>
2025-05-02 19:00:07 -0300

    Feat: add missing documentation for useThemeColor hook

--- hooks/theme/useThemeColor.ts
+++ hooks/theme/useThemeColor.ts
@@ -1,6 +1,21 @@
 import Colors from "@/hooks/theme/Colors";
 import { useColorScheme } from "react-native";
 
+/**
+ * @description This hook returns the theme color based on the current color scheme of the device.
+ * @config
+ * On the root app component, wrap the app with the ThemeProvider and set the theme based on the color scheme of the device.
+ * @example
+ * return (
+ *   <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+ *     <Stack>
+ *       {...}
+ *     </Stack>
+ *   </ThemeProvider>
+ * )
+ * @example
+ * const themeColor = useThemeColor();
+ */
 export default function useThemeColor() {
   const theme = useColorScheme() ?? "light";
 
