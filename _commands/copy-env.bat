7c6fef8229df18d97f7b75322a6caeadd9d5c600
Alberto Luti <betomdq.93@gmail.com>
2025-06-21 21:31:29 -0300

    Feat: se agrega creacion de env local pero se agrega al git ignore

--- _commands/copy-env.bat
+++ _commands/copy-env.bat
@@ -3,3 +3,10 @@ REM Copiar archivos de entorno desde la ruta base pasada como primer argumento
 copy ..\.env .\
 copy ..\.env.test .\
 copy ..\.env.production .\
+
+REM Crear archivo .env.local si no existe
+if not exist .env.local type nul > .env.local
+
+REM Agregar reglas para archivos de entorno locales al .gitignore si no existen
+findstr /C:"# local env files" .gitignore >nul 2>&1 || echo # local env files>>.gitignore
+findstr /C:".env*.local" .gitignore >nul 2>&1 || echo .env*.local>>.gitignore
